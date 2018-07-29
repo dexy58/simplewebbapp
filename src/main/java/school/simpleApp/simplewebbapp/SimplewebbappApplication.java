1a63493f4e63705ff1f82c213c29ed22458532ff
5ivek <ivan.kralj@fer.hr>
2018-07-29 11:17:37 +0200

    test

---
+++ src/main/java/school/simpleApp/simplewebbapp/SimplewebbappApplication.java
@@ -0,0 +1,12 @@
+package school.simpleApp.simplewebbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplewebbappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplewebbappApplication.class, args);
+	}
+}
